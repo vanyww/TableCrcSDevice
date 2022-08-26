ed1ab7d24cb0b0551e7782cee8aad325fe464357
Ivan <vanyww@mail.ru>
2022-08-27 01:12:27 +0300

    Add version definitions

---
+++ Inc/TableCrcSDevice/core.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SDeviceCore/core.h"
+
+/* version ************************************************************************************************************/
+
+#if (__SDEVICE_CORE_VERSION_MAJOR != 2) || (__SDEVICE_CORE_VERSION_MINOR < 2)
+#error SDevice core version is incorrect.
+#endif
+
+#define __CRC_SDEVICE_VERSION_MAJOR 1
+#define __CRC_SDEVICE_VERSION_MINOR 0
+#define __CRC_SDEVICE_VERSION_PATCH 0
+#define __CRC_SDEVICE_CORE_VERSION ((SDeviceVersion)                                                                   \
+{                                                                                                                      \
+   .Major = __CRC_SDEVICE_VERSION_MAJOR,                                                                               \
+   .Minor = __CRC_SDEVICE_VERSION_MINOR,                                                                               \
+   .Patch = __CRC_SDEVICE_VERSION_PATCH                                                                                \
+})
+
+/**********************************************************************************************************************/
