5939f3b744293b7d05f1d47ec9319d3421726520
Ivan <vanyww@mail.ru>
2022-08-27 23:11:07 +0300

    Update version to 1.0.1

--- Inc/TableCrcSDevice/core.h
+++ Inc/TableCrcSDevice/core.h
@@ -10,7 +10,7 @@
 
 #define __CRC_SDEVICE_VERSION_MAJOR 1
 #define __CRC_SDEVICE_VERSION_MINOR 0
-#define __CRC_SDEVICE_VERSION_PATCH 0
+#define __CRC_SDEVICE_VERSION_PATCH 1
 #define __CRC_SDEVICE_CORE_VERSION ((SDeviceVersion)                                                                   \
 {                                                                                                                      \
    .Major = __CRC_SDEVICE_VERSION_MAJOR,                                                                               \
