e1842d4a5058aced9ca0fb829238cdab4cc37336
Ivan <vanyww@mail.ru>
2022-08-28 15:43:33 +0300

    Update sdevice core requirement to 3.0+.x

--- Inc/TableCrcSDevice/core.h
+++ Inc/TableCrcSDevice/core.h
@@ -4,7 +4,7 @@
 
 /* version ************************************************************************************************************/
 
-#if (__SDEVICE_CORE_VERSION_MAJOR != 2) || (__SDEVICE_CORE_VERSION_MINOR < 3)
+#if (__SDEVICE_CORE_VERSION_MAJOR != 3) || (__SDEVICE_CORE_VERSION_MINOR < 0)
 #error SDevice core version is incorrect.
 #endif
 
