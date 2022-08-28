62728a1dc85af3ef92e21bb5119ffa2848b16d39
Ivan <vanyww@mail.ru>
2022-08-28 16:04:07 +0300

    Update tests

--- Test/Tests/Crc16/test.c
+++ Test/Tests/Crc16/test.c
@@ -6,7 +6,7 @@ bool TestTableCrc16SDeviceCcittFalse(void)
 {
    __SDEVICE_INIT_DATA(TableCrc16) init = { NULL, 0x1021, 0xFFFF, 0x0000, false };
    __attribute__((cleanup(__SDEVICE_DISPOSE_HANDLE(TableCrc16)))) __SDEVICE_HANDLE(TableCrc16) *handle =
-            __SDEVICE_CREATE_HANDLE(TableCrc16)(&init, NULL, 0);
+            __SDEVICE_CREATE_HANDLE(TableCrc16)(&init, NULL, NULL);
 
    uint16_t crc;
 
@@ -23,7 +23,7 @@ bool TestTableCrc16SDeviceArc(void)
 {
    __SDEVICE_INIT_DATA(TableCrc16) init = { NULL, 0x8005, 0x0000, 0x0000, true };
    __attribute__((cleanup(__SDEVICE_DISPOSE_HANDLE(TableCrc16)))) __SDEVICE_HANDLE(TableCrc16) *handle =
-            __SDEVICE_CREATE_HANDLE(TableCrc16)(&init, NULL, 0);
+            __SDEVICE_CREATE_HANDLE(TableCrc16)(&init, NULL, NULL);
 
    uint16_t crc;
 
@@ -40,7 +40,7 @@ bool TestTableCrc16SDeviceMaxim(void)
 {
    __SDEVICE_INIT_DATA(TableCrc16) init = { NULL, 0x8005, 0x0000, 0xFFFF, true };
    __attribute__((cleanup(__SDEVICE_DISPOSE_HANDLE(TableCrc16)))) __SDEVICE_HANDLE(TableCrc16) *handle =
-            __SDEVICE_CREATE_HANDLE(TableCrc16)(&init, NULL, 0);
+            __SDEVICE_CREATE_HANDLE(TableCrc16)(&init, NULL, NULL);
 
    uint16_t crc;
 
--- Test/Tests/Crc32/test.c
+++ Test/Tests/Crc32/test.c
@@ -6,7 +6,7 @@ bool TestTableCrc32SDeviceCrc32(void)
 {
    __SDEVICE_INIT_DATA(TableCrc32) init = { NULL, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true };
    __attribute__((cleanup(__SDEVICE_DISPOSE_HANDLE(TableCrc32)))) __SDEVICE_HANDLE(TableCrc32) *handle =
-            __SDEVICE_CREATE_HANDLE(TableCrc32)(&init, NULL, 0);
+            __SDEVICE_CREATE_HANDLE(TableCrc32)(&init, NULL, NULL);
 
    uint32_t crc;
 
@@ -23,7 +23,7 @@ bool TestTableCrc32SDeviceBzip2(void)
 {
    __SDEVICE_INIT_DATA(TableCrc32) init = { NULL, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, false };
    __attribute__((cleanup(__SDEVICE_DISPOSE_HANDLE(TableCrc32)))) __SDEVICE_HANDLE(TableCrc32) *handle =
-            __SDEVICE_CREATE_HANDLE(TableCrc32)(&init, NULL, 0);
+            __SDEVICE_CREATE_HANDLE(TableCrc32)(&init, NULL, NULL);
 
    uint32_t crc;
 
@@ -40,7 +40,7 @@ bool TestTableCrc32SDeviceXfer(void)
 {
    __SDEVICE_INIT_DATA(TableCrc32) init = { NULL, 0x000000AF, 0x00000000, 0x00000000, false };
    __attribute__((cleanup(__SDEVICE_DISPOSE_HANDLE(TableCrc32)))) __SDEVICE_HANDLE(TableCrc32) *handle =
-            __SDEVICE_CREATE_HANDLE(TableCrc32)(&init, NULL, 0);
+            __SDEVICE_CREATE_HANDLE(TableCrc32)(&init, NULL, NULL);
 
    uint32_t crc;
 
