70ea7cd154e7b77a50f449c9aaac4021f282c73d
Ivan <vanyww@mail.ru>
2022-08-28 16:05:33 +0300

    Rename device version definitions

--- Inc/TableCrcSDevice/core.h
+++ Inc/TableCrcSDevice/core.h
@@ -8,14 +8,14 @@
 #error SDevice core version is incorrect.
 #endif
 
-#define __CRC_SDEVICE_VERSION_MAJOR 1
-#define __CRC_SDEVICE_VERSION_MINOR 0
-#define __CRC_SDEVICE_VERSION_PATCH 1
-#define __CRC_SDEVICE_CORE_VERSION ((SDeviceVersion)                                                                   \
+#define __TABLE_CRC_SDEVICE_VERSION_MAJOR 1
+#define __TABLE_CRC_SDEVICE_VERSION_MINOR 0
+#define __TABLE_CRC_SDEVICE_VERSION_PATCH 1
+#define __TABLE_CRC_SDEVICE_CORE_VERSION ((SDeviceVersion)                                                             \
 {                                                                                                                      \
-   .Major = __CRC_SDEVICE_VERSION_MAJOR,                                                                               \
-   .Minor = __CRC_SDEVICE_VERSION_MINOR,                                                                               \
-   .Patch = __CRC_SDEVICE_VERSION_PATCH                                                                                \
+   .Major = __TABLE_CRC_SDEVICE_VERSION_MAJOR,                                                                         \
+   .Minor = __TABLE_CRC_SDEVICE_VERSION_MINOR,                                                                         \
+   .Patch = __TABLE_CRC_SDEVICE_VERSION_PATCH                                                                          \
 })
 
 /**********************************************************************************************************************/
