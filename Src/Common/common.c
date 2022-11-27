05af6eebd38e1108fe0a634026d49f14674914d7
Ivan Shestakov <vanyww@mail.ru>
2022-11-27 23:22:12 +0300

    Update UInt reversing functions using __READ_BITS macro

--- Src/Common/common.c
+++ Src/Common/common.c
@@ -7,19 +7,24 @@ uint32_t TableCrcSDeviceReverseUInt32Bits(uint32_t value)
    uint32_t mask;
 
    mask = 0b11111111111111110000000000000000;
-   value = (value & mask) >> __BIT_SIZEOF(uint32_t) / 2 | (value & ~mask) << __BIT_SIZEOF(uint32_t) / 2;
+   value = __READ_BITS(value, mask) >> __BIT_SIZEOF(uint32_t) / 2 |
+           __READ_BITS(value, ~mask) << __BIT_SIZEOF(uint32_t) / 2;
 
    mask = 0b11111111000000001111111100000000;
-   value = (value & mask) >> __BIT_SIZEOF(uint32_t) / 4 | (value & ~mask) << __BIT_SIZEOF(uint32_t) / 4;
+   value = __READ_BITS(value, mask) >> __BIT_SIZEOF(uint32_t) / 4 |
+           __READ_BITS(value, ~mask) << __BIT_SIZEOF(uint32_t) / 4;
 
    mask = 0b11110000111100001111000011110000;
-   value = (value & mask) >> __BIT_SIZEOF(uint32_t) / 8 | (value & ~mask) << __BIT_SIZEOF(uint32_t) / 8;
+   value = __READ_BITS(value, mask) >> __BIT_SIZEOF(uint32_t) / 8 |
+           __READ_BITS(value, ~mask) << __BIT_SIZEOF(uint32_t) / 8;
 
    mask = 0b11001100110011001100110011001100;
-   value = (value & mask) >> __BIT_SIZEOF(uint32_t) / 16 | (value & ~mask) << __BIT_SIZEOF(uint32_t) / 16;
+   value = __READ_BITS(value, mask) >> __BIT_SIZEOF(uint32_t) / 16 |
+           __READ_BITS(value, ~mask) << __BIT_SIZEOF(uint32_t) / 16;
 
    mask = 0b10101010101010101010101010101010;
-   value = (value & mask) >> __BIT_SIZEOF(uint32_t) / 32 | (value & ~mask) << __BIT_SIZEOF(uint32_t) / 32;
+   value = __READ_BITS(value, mask) >> __BIT_SIZEOF(uint32_t) / 32 |
+           __READ_BITS(value, ~mask) << __BIT_SIZEOF(uint32_t) / 32;
 
    return value;
 }
