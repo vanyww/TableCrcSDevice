fa9d18cba254ae2d09e2b9d473e56709ec056d56
Ivan Shestakov <vanyww@mail.ru>
2022-11-27 23:21:39 +0300

    Update sdevice core requirement to 4.4+.x

--- Inc/TableCrcSDevice/core.h
+++ Inc/TableCrcSDevice/core.h
@@ -4,7 +4,7 @@
 
 /* version ************************************************************************************************************/
 
-#if (__SDEVICE_CORE_VERSION_MAJOR != 4) || (__SDEVICE_CORE_VERSION_MINOR < 2)
+#if (__SDEVICE_CORE_VERSION_MAJOR != 4) || (__SDEVICE_CORE_VERSION_MINOR < 4)
 #error SDevice core version is incorrect.
 #endif
 
