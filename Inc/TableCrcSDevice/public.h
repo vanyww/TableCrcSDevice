d0e7906cc2302d2bae46d264a2c1851014fe06f4
Ivan Shestakov <vanyww@mail.ru>
2023-05-14 18:47:45 +0300

    Set project version to 4.0.0

--- Inc/TableCrcSDevice/public.h
+++ Inc/TableCrcSDevice/public.h
@@ -2,9 +2,9 @@
 
 #include "SDeviceCore/core.h"
 
-#define TABLE_CRC_SDEVICE_VERSION_MAJOR 3
-#define TABLE_CRC_SDEVICE_VERSION_MINOR 1
-#define TABLE_CRC_SDEVICE_VERSION_PATCH 1
+#define TABLE_CRC_SDEVICE_VERSION_MAJOR 4
+#define TABLE_CRC_SDEVICE_VERSION_MINOR 0
+#define TABLE_CRC_SDEVICE_VERSION_PATCH 0
 #define TABLE_CRC_SDEVICE_CORE_VERSION ((SDeviceVersion)                                                               \
 {                                                                                                                      \
    .Major = TABLE_CRC_SDEVICE_VERSION_MAJOR,                                                                           \
