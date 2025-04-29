bdbcbd898ba9e9632cc13f0bcb54c5998648812e
Avvesey Erockhov <kolyann088@gmail.com>
2025-04-29 11:17:22 +0300

    Set version to 5.0.0

--- Inc/TableCrcSDevice/public_base.h
+++ Inc/TableCrcSDevice/public_base.h
@@ -3,7 +3,7 @@
 #include "config_base.h"
 #include "dependencies_base.h"
 
-#define TABLE_CRC_SDEVICE_VERSION_MAJOR 4
+#define TABLE_CRC_SDEVICE_VERSION_MAJOR 5
 #define TABLE_CRC_SDEVICE_VERSION_MINOR 0
 #define TABLE_CRC_SDEVICE_VERSION_PATCH 0
 
